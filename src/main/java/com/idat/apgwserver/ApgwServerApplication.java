c8b069f7ef13e69b9f27cf18805d24e7ed647fb2
Wilder <scoutingcreative@gmail.com>
2022-02-03 20:14:55 -0500

    initial commit

---
+++ src/main/java/com/idat/apgwserver/ApgwServerApplication.java
@@ -0,0 +1,15 @@
+package com.idat.apgwserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApgwServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApgwServerApplication.class, args);
+	}
+
+}
